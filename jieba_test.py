a04bcccea372cd5a31d3b3546eced6e83e5c958c
litianjiu9999 <1216034980@qq.com>
2022-02-13 09:47:29 +0800

    “merge”

--- jieba_test.py
+++ jieba_test.py
@@ -4,3 +4,10 @@ text = "我今天吃了六个豆沙包，早上、中午、晚上各两个。"
 seg = jieba.cut(text)  
 print(' '.join(seg)) 
 ##test
+
+
+import jieba.posseg as posseg
+text = "一天不敲代码我就浑身难受"
+# 形如pair('word, 'pos')的结果
+seg = posseg.cut(text)  
+print([se for se in seg]) 
